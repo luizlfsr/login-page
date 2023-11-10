4ad8591e1ac073b96229016da347ffafb8984908
Luiz Felipe <139113407+luizlfsr@users.noreply.github.com>
2023-11-09 23:36:07 -0300

    Update script.js
    adicionada a verificação da senha, e correção de bugs em caixas de texto

--- script.js
+++ script.js
@@ -16,6 +16,11 @@ document.addEventListener('DOMContentLoaded', function () {
             return emailRegex.test(email);
         }
 
+        function isStrongPassword(passowrd) {
+            const passwordregex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,}$/;
+            return passwordregex.test(passowrd);
+        }
+
         emailError.textContent = '';
         passwordError.textContent = '';
 
@@ -29,7 +34,9 @@ document.addEventListener('DOMContentLoaded', function () {
             emailError.textContent = 'Por favor, preencha o campo com um formato de e-mail valido'
             emailError.style.display = 'block'
         }else {
-            document.getElementById('email').style.borderColor = 'rgba(255, 255, 255, .25)'
+            document.getElementById('email').style.borderColor = 'rgba(2, 255, 25, 1)'
+            emailError.style.display = 'none'
+            emailError.textContent = '';
         }
 
 
@@ -37,10 +44,20 @@ document.addEventListener('DOMContentLoaded', function () {
         /*passowrd verification*/
         if (password  === '') {
             document.getElementById('password').style.borderColor = '#f75454'
-            passwordError.textContent = 'Por favor, preencha o campo de senha'
+            passwordError.textContent = 'Por favor, preencha o campo de senha!'
             passwordError.style.display = 'block'
-        } else {
-            document.getElementById('password').style.borderColor = 'rgba(255, 255, 255, .25)'
+        } else if (!isStrongPassword(password)) {
+            document.getElementById('password').style.borderColor = '#f7545'
+            passwordError.textContent = 'A senha deve conta 8 digito, letras maiusculas e minusculas, números e caracteres especiais'
+        } else if (isStrongPassword && email === '') {
+            passwordError.style.display = 'none'
+            passwordError.textContent = '';
+            emailError.style.display = 'block'
+            emailError.textContent = 'Por favor, preencha o campo de e-mail!';
+        }  else {
+            document.getElementById('password').style.borderColor = 'rgba(2, 255, 25, 1)'
+            passwordError.style.display = 'none'
+            passwordError.textContent = '';
         }
 
 
